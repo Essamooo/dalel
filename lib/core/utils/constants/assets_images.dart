ed2a1ca1214823c23f31bb2ee89aaaf2b9a2d8fc
Essamooo <essam.gharib77@gmail.com>
2024-04-07 04:11:12 +0200

    Add go_router dependency version 13.2.2
    Added go_router dependency version 13.2.2 to pubspec.lock and updated pubspec.yaml to include go_router version 13.2.2 as a dependency.

---
+++ lib/core/utils/constants/assets_images.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgot_password.png
+  static const String imagesForgotPassword = "assets/images/forgot_password.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesOrderSuccess
+  /// assets/images/order_success.png
+  static const String imagesOrderSuccess = "assets/images/order_success.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
---
+++ lib/core/utils/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._internal();
+
+  static const Color primaryColor = Color(0xffC49E85);
+  static const Color offWhite = Color(0xffF8F4F9);
+  static const Color deepBrown = Color(0xff6B4B3E);
+  static const Color gray = Color(0xffC4C4C4);
+  static const Color deepGray = Color(0xff6F6460);
+  static const Color lightGray = Color(0xffB4B4B4);
+  static const Color black = Color(0xff333333);
+}
